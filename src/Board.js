d7f03229b81b431c50afe172b4394601c709382c
Christopher Monahan <17766780+chris-monahan@users.noreply.github.com>
2020-06-15 18:17:08 +0100

    Updating style and code structure
    - Using css grid for board grid now
    - Removing pointless square component
    - Board grid now expands to fill screen space and has updated look

--- src/Board.js
+++ src/Board.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import Square from './Square';
 
 class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={this.props.squares[i]}
-              onClick={() => this.props.onClick(i)}/>;
+    renderSquare(i, placeFlags) {
+
+      let classString = "squareWrapper" + 
+            (placeFlags.colTop ? ' colTop' : '') +
+            (placeFlags.colBottom ? ' colBottom' : '') +
+            (placeFlags.rowStart ? ' rowStart' : '') +
+            (placeFlags.rowEnd ? ' rowEnd' : '');
+
+      return <div id={"boardSquare_" + i} className={classString}>
+        <button className="square" onClick={() => this.props.onClick(i)}>
+          {this.props.squares[i]}
+        </button>
+      </div>;
+    }
+
+    renderRow(length, squareIndex, placeFlags, wrap){
+      let rowSquares = [];
+      for(let i = 0; i < length; i++){
+        rowSquares[i] = this.renderSquare(squareIndex + i, Object.assign({
+          rowStart: i === 0, 
+          rowEnd: i === (length - 1)
+        }, placeFlags));
+      }
+
+      if(wrap === true){
+        return <div className="boardRow">{rowSquares}</div>
+      } else{
+        return rowSquares
+      }
+
+      
     }
   
     render() {
       return (
         <div class="board">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {this.renderRow(3, 0, {colTop: true, colBottom: false})}
+            {this.renderRow(3, 3, {colTop: false, colBottom: false})}
+            {this.renderRow(3, 6 ,{colTop: false, colBottom: true})}
         </div>
       );
     }
