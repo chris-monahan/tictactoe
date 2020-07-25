1a20bc9a6a9195d354bc2d366dbd714a5fbc63a3
Christopher Monahan <17766780+chris-monahan@users.noreply.github.com>
2020-07-25 23:49:47 +0100

    Set style properties at init

--- src/init.js
+++ src/init.js
@@ -1,5 +1,8 @@
-function init(){
-
+function init(config){
+    let root = document.documentElement;
+    root.style.setProperty("--grid-x-num", config.board.sizeX);
+    root.style.setProperty("--grid-y-num", config.board.sizeY);
+    root.style.setProperty("--sidebar-bg-color", config.sidebar.backgroundColor)
 }
 
-export default init;
+export default init; 
