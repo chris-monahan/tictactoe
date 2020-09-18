434b7802bf68ea5d1af419cdd640ff5de1276da0
Christopher Monahan <17766780+chris-monahan@users.noreply.github.com>
2020-09-18 02:59:06 +0100

    Refactor of board.js

--- src/UIComponents/Board.js
+++ src/UIComponents/Board.js
@@ -3,58 +3,18 @@ import cross from "../cross.svg";
 import nought from "../nought.svg"
 
 class Board extends React.Component {
-    renderSquare(pointX, pointY, placeFlags) {
+    
 
-      let squareIndex = (pointY * this.props.squares.length) + pointX;
-
-      let classString = "squareWrapper" + 
-            (placeFlags.colTop ? ' colTop' : '') +
-            (placeFlags.colBottom ? ' colBottom' : '') +
-            (placeFlags.rowStart ? ' rowStart' : '') +
-            (placeFlags.rowEnd ? ' rowEnd' : '');
-
-      let playPiece;
-
-      if (this.props.squares.getSquareVal(pointX, pointY) === 'X'){
-        playPiece = <img src={cross} class="playPiece cross"></img>  
-      } else if (this.props.squares.getSquareVal(pointX, pointY) === 'O') {
-        playPiece = <img src={nought} class="playPiece nought"></img>
-      } else {
-        playPiece = <div class="playPiece blank"></div>
-      }
-
-      return <div id={"boardSquare_" + squareIndex} className={classString}>
-        <button className="square" onClick={() => this.props.onClick(pointX, pointY, squareIndex)}>
-          {playPiece}
-        </button>
-      </div>;
-    }
-
-    renderRow(length, rowNum, placeFlags, wrap){
-      let rowSquares = [];
-      for(let i = 0; i < length; i++){
-        rowSquares[i] = this.renderSquare(i + 1, rowNum + 1, Object.assign({
-          rowStart: i === 0, 
-          rowEnd: i === (length - 1)
-        }, placeFlags));
-      }
-
-      if(wrap === true){
-        return <div className="boardRow">{rowSquares}</div>
-      } else{
-        return rowSquares
-      }
-
-      
-    }
+   
   
     render() {
+      let thisBoard = this;
       let squares = this.props.squares; 
       let rows = [];
 
       for(let i = 0; i < squares.sizeX; i++){
           let placeFlags = {colTop: i === 0, colBottom: i === squares.length}
-          rows[i] = this.renderRow(squares.sizeY, i, placeFlags);
+          rows[i] = renderRow(squares.sizeY, i, placeFlags);
       }
 
       return (
@@ -62,6 +22,51 @@ class Board extends React.Component {
             {rows}
         </div>
       );
+
+      function renderSquare(pointX, pointY, placeFlags) {
+
+        let squareIndex = (pointY * squares.length) + pointX;
+  
+        let classString = "squareWrapper" + 
+              (placeFlags.colTop ? ' colTop' : '') +
+              (placeFlags.colBottom ? ' colBottom' : '') +
+              (placeFlags.rowStart ? ' rowStart' : '') +
+              (placeFlags.rowEnd ? ' rowEnd' : '');
+  
+        let playPiece;
+  
+        if (squares.getSquareVal(pointX, pointY) === 'X'){
+          playPiece = <img src={cross} class="playPiece cross"></img>  
+        } else if (squares.getSquareVal(pointX, pointY) === 'O') {
+          playPiece = <img src={nought} class="playPiece nought"></img>
+        } else {
+          playPiece = <div class="playPiece blank"></div>
+        }
+  
+        return <div id={"boardSquare_" + squareIndex} className={classString}>
+          <button className="square" onClick={() => thisBoard.props.onClick(pointX, pointY, squareIndex)}>
+            {playPiece}
+          </button>
+        </div>;
+      }
+
+      function renderRow(length, rowNum, placeFlags, wrap){
+        let rowSquares = [];
+        for(let i = 0; i < length; i++){
+          rowSquares[i] = renderSquare(i + 1, rowNum + 1, Object.assign({
+            rowStart: i === 0, 
+            rowEnd: i === (length - 1)
+          }, placeFlags));
+        }
+  
+        if(wrap === true){
+          return <div className="boardRow">{rowSquares}</div>
+        } else{
+          return rowSquares
+        }
+  
+        
+      }
     }
   }
 
